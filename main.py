2c7703ea50cb5ddb26f389f592dc793829523d37
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-02-19 14:47:37 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
-from pyrogram import Client, filters
+import urllib.parse
 import boto3
-from urllib.parse import quote
+from botocore.exceptions import ClientError
+from pyrogram import Client, filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -15,12 +16,16 @@ BOT_TOKEN = "6752618609:AAFJufwcSl-i3CqOXjO4SXGi5NdHYP9BuVo"
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # S3 Configuration
-s3 = boto3.client('s3',
-                  aws_access_key_id='31348d1b8b734ada5dc9fc302782d68c',
-                  aws_secret_access_key='cdab26813ba4eefc74ce4da0b93e4243',
-                  endpoint_url='https://sin1.contabostorage.com')
+S3_CONFIG = {
+    'type': 's3',
+    'provider': 'Ceph',
+    'access_key_id': '31348d1b8b734ada5dc9fc302782d68c',
+    'secret_access_key': 'cdab26813ba4eefc74ce4da0b93e4243',
+    'endpoint': 'https://sin1.contabostorage.com',
+}
 
-bucket_name = 'movieslist'  # Bucket name in your contabostorage server
+# Initialize S3 client
+s3 = boto3.client('s3', **S3_CONFIG)
 
 # Define the function to handle the /start command
 @app.on_message(filters.command("start"))
@@ -32,15 +37,19 @@ async def handle_video(client, message):
     # Download the video file
     file_path = await message.download()
     
-    # Upload the video file to the contabostorage server
-    file_name = file_path.split('/')[-1]  # Extracting file name from the path
-    s3.upload_file(file_path, bucket_name, file_name)
-    
-    # Generate HTTPS link
-    https_link = f"https://sin1.contabostorage.com/{bucket_name}/{quote(file_name)}"
-    
-    # Respond with the converted HTTPS link
-    await message.reply_text(f"Here's the HTTPS link for your video: {https_link}")
+    try:
+        # Upload the file to S3-compatible storage
+        file_name = file_path.split("/")[-1]  # Extracting file name
+        s3.upload_file(file_path, 'movieslist', file_name)  # Upload to bucket 'movieslist'
+        
+        # Generate HTTPS link
+        https_link = f"https://sin1.contabostorage.com/movieslist/{urllib.parse.quote(file_name)}"
+        
+        # Respond with the generated HTTPS link
+        await message.reply_text(f"Here's the HTTPS link for your video: {https_link}")
+    except ClientError as e:
+        logger.error(f"Failed to upload file to S3: {e}")
+        await message.reply_text("Sorry, there was an error processing your request.")
 
 # Start the Bot
 if __name__ == '__main__':
