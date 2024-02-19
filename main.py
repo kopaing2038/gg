dc398e540fe493e1e0db23445eb88cd18321c532
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-02-19 13:25:56 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,43 @@
+
+import requests
 import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from pyrogram import Client, filters
 import requests
 
-# Set up logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Define your bot token
-TOKEN = "6752618609:AAFJufwcSl-i3CqOXjO4SXGi5NdHYP9BuVo"
+
+API_ID = '28679600'
+API_HASH = '3d54e9caa473c838ed8762256afcb176'
+BOT_TOKEN = "6752618609:AAFJufwcSl-i3CqOXjO4SXGi5NdHYP9BuVo"
+
+# Create the Pyrogram client
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Define the function to handle the /start command
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! Send me a video file.')
+@app.on_message(filters.command("start"))
+def start(client, message):
+    message.reply_text('Hi! Send me a video file.')
 
-# Define the function to handle video messages
-def handle_video(update: Update, context: CallbackContext) -> None:
-    video_file = update.message.video.get_file()
-    file_id = video_file.file_id
-    file_path = video_file.file_path
-    file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+@app.on_message(filters.video)
+def handle_video(client, message):
+    file_id = message.video.file_id
+    file_path = client.get_download_location(file_id)
+    file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}"
 
     # Convert video file to HTTPS link
     response = requests.get(file_url)
     if response.status_code == 200:
-        https_link = "https://stream.moviesmaster.org//converted_video.mp4"  # Replace with your HTTPS link
-        update.message.reply_text(f"Here's the HTTPS link for your video: {https_link}")
+        https_link = "https://stream.moviesmaster.org/converted_video.mp4"  # Replace with your HTTPS link
+        message.reply_text(f"Here's the HTTPS link for your video: {https_link}")
     else:
-        update.message.reply_text("Failed to convert the video.")
+        message.reply_text("Failed to convert the video.")
 
-# Define the main function to run the bot
-def main() -> None:
-    # Create the Updater and pass it your bot's token
-    updater = Updater(TOKEN)
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-
-    # Register command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Register message handler for videos
-    dispatcher.add_handler(MessageHandler(Filters.video, handle_video))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
-    updater.idle()
-
+# Start the Bot
 if __name__ == '__main__':
-    main()
+    app.run()
+
+
